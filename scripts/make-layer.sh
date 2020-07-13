5542732ac3da38c2cbf82e761ce4b37d2769bd52
Justin Tunis <jtunis21@gmail.com>
2020-07-13 17:49:23 -0400

    add shebang and exit codes

--- scripts/make-layer.sh
+++ scripts/make-layer.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # parse inputs
 for arg in "$@"
 do
@@ -6,7 +8,7 @@ do
         --torch=*) TORCH_VERSION="${arg#*=}" shift;;
         --torchvision=*) TORCHVISION_VERSION="${arg#*=}" shift;;
         --torchaudio=*) TORCHAUDIO_VERSION="${arg#*=}" shift;;
-        *) echo "ERROR: Invalid argument ${arg}" && exit;;
+        *) echo "ERROR: Invalid argument ${arg}" && exit 1;;
     esac
 done
 
@@ -18,13 +20,13 @@ case ${PYTHON_VERSION} in
     3.7) SHORT_PYTHON_VERSION="cp37";;
     3.8) SHORT_PYTHON_VERSION="cp38";;
     3.9) SHORT_PYTHON_VERSION="cp39";;
-    "") echo "ERROR: No Python version specified" && exit;;
-    *) echo "ERROR: Invalid Python version. Expected versions include: 2.7, 3.5, 3.6, 3.7, 3.8, 3.9" && exit;;
+    "") echo "ERROR: No Python version specified" && exit 1;;
+    *) echo "ERROR: Invalid Python version. Expected versions include: 2.7, 3.5, 3.6, 3.7, 3.8, 3.9" && exit 1;;
 esac
 
 # build torch wheel URL
 case ${TORCH_VERSION} in
-    "") "ERROR: No Torch version specified" && exit;;
+    "") "ERROR: No Torch version specified" && exit 1;;
     *) TORCH_WHEEL="https://download.pytorch.org/whl/cpu/torch-${TORCH_VERSION}%2Bcpu-${SHORT_PYTHON_VERSION}-${SHORT_PYTHON_VERSION}-linux_x86_64.whl";;
 esac
 
@@ -52,10 +54,10 @@ docker run -v "$PWD":/var/task "lambci/lambda:build-python${PYTHON_VERSION}" /bi
 ${TORCH_WHEEL} \
 ${TORCHVISION_WHEEL} \
 ${TORCHAUDIO_WHEEL} \
--t python/lib/python${PYTHON_VERSION}/site-packages; exit" || exit
+-t python/lib/python${PYTHON_VERSION}/site-packages; exit" || exit 1
 
 # remove extraneous files and directories
-cd "python/lib/python${PYTHON_VERSION}/site-packages" || exit
+cd "python/lib/python${PYTHON_VERSION}/site-packages" || exit 1
 find . -type d -name "test*" -exec rm -rf {} +
 find . -type d -name "__pycache__" -exec rm -rf {} +
 rm -rf ./{caffe2,wheel,wheel-*,pkg_resources,boto*,aws*,pip,pip-*,pipenv,setuptools}
@@ -67,7 +69,7 @@ zip -r9 requirements.zip torch
 rm -rf torch
 
 # add unzip_requirements module
-cd "../../../.." || exit
+cd "../../../.." || exit 1
 cp -r unzip_requirements python/lib/python${PYTHON_VERSION}/site-packages
 
 # zip packages
