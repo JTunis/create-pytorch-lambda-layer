b89147a856128d62beace0a13fd585c87205559a
Richard Waller <richard.waller@cazoo.co.uk>
2022-11-16 12:37:45 +0000

    Don't use PIL

--- src/main.py
+++ src/main.py
@@ -5,26 +5,20 @@ except ImportError:
 import torch
 import torchvision
 import torchvision.transforms as transforms
+from torchvision.io import read_image
 
 import boto3
 import json
-
-# import PIL.Image as Image
-
-# from io import BytesIO
-# import base64
+import base64
 
 # transforms for the input image
-loader = transforms.Compose([transforms.Resize((400, 400)),
+loader = transforms.Compose([transforms.ToPILImage(),
+    transforms.Resize((400, 400)),
                                 transforms.ToTensor(),
                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 
 def main(event, context):
-    # tensor = torch.rand((2, 5))
-    # print(tensor)
-    
-    
     s3 = boto3.client('s3')
 
     bucket = 'data-hello-world-car-gra-serverlessdeploymentbuck-1lieokb7ux9p9'
@@ -39,40 +33,39 @@ def main(event, context):
     # switch the model to evaluation mode to make dropout and batch norm work in eval mode
     model_ft.eval()
     
-    # print('model_ft')
-    # print(model_ft)
-    
     body = json.loads(event['body'])
     
     base64_encoded_image = body['image']
     
-    # print("base64_encoded_image")
-    # print(base64_encoded_image)
+    img_path = '/tmp/img.jpg'
+    
+    with open(img_path, "wb") as file:
+        print(file.write(base64.b64decode(base64_encoded_image)))
 
-    # image = Image.open(BytesIO(base64.b64decode(img)))
+    image = read_image(img_path)
     # print('image')
     # print(image)
-    # image = loader(image).float()
-    # image = torch.autograd.Variable(image, requires_grad=True)
-    # image = image.unsqueeze(0)
-    # output = model_ft(image)
-    # conf, predicted = torch.max(output.data, 1)
+    image = loader(image).float()
+    image = torch.autograd.Variable(image, requires_grad=True)
+    image = image.unsqueeze(0)
+    output = model_ft(image)
+    conf, predicted = torch.max(output.data, 1)
 
-    # print('confidence:', conf.item())
-    # print('predicted:', predicted.item())
+    print('confidence:', conf.item())
+    print('predicted:', predicted.item())
     
-    # # map output to label enum
-    # predictionMap = {0: 'Excellent', 1: 'Good', 2: 'Average'} 
-    # grade = predictionMap[predicted.item()]
-    # print('grade', grade)  
+    # map output to label enum
+    predictionMap = {0: 'Excellent', 1: 'Good', 2: 'Average'} 
+    grade = predictionMap[predicted.item()]
+    print('grade', grade)  
 
     
-    # # TODO save output to S3
-    # # s3.put(output, 's3-url')
+    # TODO save output to S3
+    # s3.put(output, 's3-url')
 
     body = {
         "input": event,
-        # "output": grade
+        "output": grade
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
